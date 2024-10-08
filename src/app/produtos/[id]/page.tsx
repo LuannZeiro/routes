6684d2bfcd303978fd2e7d23969aa7a83c86d3d4
Luann Mariano <luann.mariano2003@gmail.com>
2024-10-07 22:42:59 -0300

    First Commit

---
+++ src/app/produtos/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { Menu } from "../../components/menu/Menu"
+export default function Detalhes() {
+    return (
+        <div>
+            <h1>Detalhes</h1>
+            <Menu />
+        </div>
+    )
+}
---
+++ src/sobre/page.tsx
@@ -0,0 +1,10 @@
+import { Menu } from "../app/components/menu/Menu"
+
+export default function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <Menu />
+        </div>
+    )
+}
